f190fefd3e77571b5278c9aa41f9630f8e9b811f
Rodrigo <rodrigo.noschang1@gmail.com>
2022-11-21 19:09:24 -0300

    Changing cors origin problem and adding .env file with the url that should be able to access it

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 
 const app = express();
 app.use(cors());
 const httpServer = http.createServer(app);
+dotenv.config();
+
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: process.env.ALLOWED_CORS_ORIGIN,
         methods: ['POST', 'GET'],
         allowedHeaders: ['checkers-header'],
         credentials: true
